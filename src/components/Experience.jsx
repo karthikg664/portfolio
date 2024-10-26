747608535c489b207fabbba247ea7db71eb665fc
kartik <59840735+karthikg664@users.noreply.github.com>
2024-10-26 16:19:04 +0530

    in this commit there are some errors like :- 3d computer light error, &nbps showing, projects cards not showing as expected, slow loading of 3d models, slow process of tech balls. other than these errors every thing looks and works fine.

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import { motion } from 'framer-motion';
 import 'react-vertical-timeline-component/style.min.css';
@@ -7,7 +7,8 @@ import { experiences } from '../constants';
 import { SectionWrapper } from '../hoc';
 import { textVariant } from '../utils/motion';
 
-const ExperienceCard = ({ experience }) => (
+const ExperienceCard = ({ experience }) => {
+  return (
   <VerticalTimelineElement
     contentStyle={{ background: '#1d1836', color: '#fff' }}
     contentArrowStyle={{ borderRight: '7px solid #232631' }}
@@ -46,12 +47,13 @@ const ExperienceCard = ({ experience }) => (
     </ul>
   </VerticalTimelineElement>
 )
+}
 const Experience = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>What I have done so far</p>
-        <h2 className={styles.sectionHeadText}>Work Experience.</h2>
+        <p className={`${styles.sectionSubText} text-center`}>What I have done so far</p>
+        <h2 className={`${styles.sectionHeadText} text-center`}>Work Experience.</h2>
       </motion.div>
       <div className="mt-20 flex flex-col">
         <VerticalTimeline>
@@ -60,10 +62,8 @@ const Experience = () => {
           ))}
         </VerticalTimeline>
       </div>
-
-
     </>
-  )
-}
+  );
+};
 
 export default SectionWrapper(Experience, "Work");
